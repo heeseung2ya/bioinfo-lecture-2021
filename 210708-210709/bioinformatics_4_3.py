8c4d290b1117ba86d5b647e735c4de7c8e768576
heeseung2ya <69632673+heeseung2ya@users.noreply.github.com>
2021-10-18 03:34:12 +0900

    Update 2021.10.18.

--- 210708-210709/bioinformatics_4_3.py
+++ 210708-210709/bioinformatics_4_3.py
@@ -1,44 +1,12 @@
-# with open 쓰지말고 깔끔하게 다시 짜보기
+fr = open('sequence.protein.gb', 'r')
 
-ORIGIN_idx = 0
-seq = ""
+title = fr.readline().strip()
+while 1:
+    if fr.readline().strip().startswith('ORIGIN'): break
 
-file = open("sequence.protein.gb", "r").readlines()
-for line in file:
-    if line.strip() == file[0].strip():
-        title = line.strip()
-    if ORIGIN_idx == 1:
-        seq += "".join(line.strip().split()[1:])
-    elif line.strip() == "ORIGIN":
-        ORIGIN_idx = 1
-open("sequence.protein.gb", "r").close()
+seq = ''
+for line in fr:
+    seq += ''.join(line.lstrip().split()[1:])
 
-for i in range(0, len(seq) + 1, 70):
-    print(seq[i : i + 70])
-
-
-"""
-# 수정 전 코드
-title = ""
-seq = ""
-idx = 0
-cnt = 0
-tmp2 = []
-
-with open("sequence.protein.gp", "r") as fr:
-    for line in fr.readlines():
-        if cnt == 0:
-            title = line.strip()
-        tmp = line.strip()
-        if tmp.startswith("ORIGIN"):
-            idx = 1
-        elif idx == 1:
-            tmp2.append("".join(line.strip().split()[1:]))
-            seq = "".join(tmp2).strip()
-        cnt += 1
-
-for i in range(len(seq)):
-    print(seq[i], end="")
-    if (i + 1) % 70 == 0:
-        print("\n", end="")
-"""
+for i in range(0, len(seq), 70):
+    print(seq[i:i+70])
