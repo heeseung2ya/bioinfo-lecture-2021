726ee4cae27959b3f5dcbaf15c8b549439136a91
heeseung2ya <69632673+heeseung2ya@users.noreply.github.com>
2021-10-18 03:15:04 +0900

    Update 2021.10.18.

--- 210708-210709/bioinformatics_4_1.py
+++ 210708-210709/bioinformatics_4_1.py
@@ -1,40 +1,12 @@
-# with open 쓰지말고 깔끔하게 다시 짜보기
+fr = open('sequence.protein.gb', 'r')
 
-ORIGIN_idx = 0
-seq = ""
+title = fr.readline().strip()
+while 1:
+    if fr.readline().strip().startswith('ORIGIN'): break
 
-file = open("sequence.protein.gb", "r").readlines()
-for line in file:
-    if line.strip() == file[0].strip():
-        title = line.strip()
-    if ORIGIN_idx == 1:
-        seq += line.lstrip()
-    elif line.strip() == "ORIGIN":
-        ORIGIN_idx = 1
-open("sequence.protein.gb", "r").close()
+seq = ''
+for line in fr:
+    seq += line.lstrip()
 
-print(f"title: {title}")
-print(f"seq: {seq}")
-
-
-"""
-# 수정 전 코드
-title = ""
-seq = ""
-idx = 0
-cnt = 0
-
-with open("sequence.protein.gb", "r") as fr:
-    for line in fr.readlines():
-        tmp = line.strip()
-        if cnt == 0:
-            title = line.strip()
-        if tmp.startswith("ORIGIN"):
-            idx = 1
-        elif idx == 1:
-            seq += line.lstrip()
-        cnt += 1
-seq = seq.strip()
-print(f"title: {title}")
-print(f"seq: {seq}")
-"""
+print('title:', title)
+print('seq:', seq)
