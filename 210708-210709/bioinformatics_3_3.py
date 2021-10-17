54e6de1ab4dcec15066705cd5a6956c4d16f94b7
heeseung2ya <69632673+heeseung2ya@users.noreply.github.com>
2021-10-18 02:27:38 +0900

    Update 2021.10.18.

--- 210708-210709/bioinformatics_3_3.py
+++ 210708-210709/bioinformatics_3_3.py
@@ -1,6 +1,3 @@
-with open("sequence.protein.fasta", "r") as fr:
-    for line in fr.readlines():
-        if line.startswith(">"):
-            print(line.strip())
-        else:
-            print(line.strip(), end="")
+fr = open('sequence.protein.fasta')
+for line in fr:
+    print(line.strip())
