4603245c0eadae35be0dc3a0a76c10f50c18c85c
heeseung2ya <69632673+heeseung2ya@users.noreply.github.com>
2021-10-18 18:51:59 +0900

    Update 2021.10.18.

--- 210708-210709/bioinformatics_4_4.py
+++ 210708-210709/bioinformatics_4_4.py
@@ -1,51 +1,68 @@
-input_name = input("Enter input file: ")
-output_name = input("Enter output file: ")
-print(
-    "\n\
-Option-1) Read a FASTA format DNA sequence file and make a reverse sequence file.\n\
-Option-2) Read a FASTA format DNA sequence file and make a reverse complement sequence file.\n\
-Option-3) Convert GenBank format file to FASTA format file."
-)
+def menu():
+    print('Option-1) Read a FASTA format DNA sequence file and make a reverse sequence file.')
+    print('Option-2) Read a FASTA format DNA sequence file and make a reverse complement sequence file.')
+    print('Option-3) Convert GenBank format to FASTA format file.')
+    op = input('Select the option: ')
+    return op
 
-select = int(input("Select the option: "))
 
-seq = ""
-fasta_file = open(input_name, "r").readlines()
-for line in fasta_file:
-    if line.startswith(">"):
-        header = line
-    else:
+def op1(fr, fw):
+    title = fr.readline().strip()
+    seq = ''
+    for line in fr:
         seq += line.strip()
-open(input_name, "r").close()
 
-rev_seq = seq[::-1]
-if select == 1:
-    with open(output_name, "w") as file:
-        file.write(header)
-        file.write(rev_seq)
-elif select == 2:
-    d_rev_com = {"A": "T", "C": "G", "G": "C", "T": "A"}
-    rev_com_seq = ""
-    for base in rev_seq:
-        rev_com_seq += d_rev_com[base]
-    with open(output_name, "w") as file:
-        file.write(header)
-        file.write(rev_com_seq)
-elif select == 3:
-    GBtoFASTA_header = ">"
-    GBtoFASTA_seq = ""
+    rev = seq[::-1]
+    fw.write(title)
+    for i in range(0, len(rev), 70):
+        fw.write('\n' + rev[i:i+70])
 
-    file = open(input_name, "r").readlines()
-    ORIGIN_idx = 0
-    for line in file:
-        if line.strip() == file[0].strip():
-            GBtoFASTA_header += line.strip() + "\n"
-        if ORIGIN_idx == 1:
-            GBtoFASTA_seq += "".join(line.strip().split()[1:])
-        elif line.strip() == "ORIGIN":
-            ORIGIN_idx = 1
-    open(input_name, "r").close()
+    
+def op2(fr, fw):
+    title = fr.readline()
+    seq = ''
+    for line in fr:
+        seq += line.strip()
+    
+    rev = seq[::-1]
+    d_complement = {'A':'T', 'C':'G', 'G':'C', 'T':'A'}
+    fw.write(title)
+    cnt = 0
+    for i in rev:
+        cnt += 1
+        fw.write(d_complement[i])
+        if cnt % 70 == 0: fw.write('\n')
 
-    with open(output_name, "w") as file:
-        file.write(GBtoFASTA_header)
-        file.write(GBtoFASTA_seq)
+
+def op3(fr, fw):
+    title = '>' + fr.readline().strip()
+    while 1:
+        if fr.readline().strip().startswith('ORIGIN'): break
+    seq = ''
+    for line in fr:
+        seq += ''.join(line.strip().split()[1:])
+    
+    fw.write(title)
+    cnt = 0
+    for i in range(0, len(seq), 70):
+        cnt += 1
+        fw.write('\n' + seq[i:i+70])
+
+
+def main():
+    # fr = open(input('Enter input file: '), 'r')
+    # fw = open(input('Enter output file: '), 'w')
+    fr = open('sequence.nucleotide.gb', 'r')
+    fw = open('new.sequence.nucleotide.fasta', 'w')
+    op = menu()
+    if op == '1': op1(fr, fw)
+    elif op == '2': op2(fr, fw)
+    elif op == '3': op3(fr, fw)
+    else: exit()
+
+    fr.close()
+    fw.close()
+
+
+if __name__ == "__main__":
+    main()
